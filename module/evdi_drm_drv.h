4cbe384c823f8058fbccd17079a23226614ceb77
Richard Gieg <richgieg@users.noreply.github.com>
2026-02-28 16:36:58 -0800

    Fix build on CentOS 9 and 10

--- module/evdi_drm_drv.h
+++ module/evdi_drm_drv.h
@@ -102,7 +102,7 @@ long evdi_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 struct drm_framebuffer *evdi_fb_user_fb_create(
 				struct drm_device *dev,
 				struct drm_file *file,
-#if KERNEL_VERSION(6, 17, 0) <= LINUX_VERSION_CODE
+#if KERNEL_VERSION(6, 17, 0) <= LINUX_VERSION_CODE || defined(CENTOS9) || defined(CENTOS10)
 				const struct drm_format_info *info,
 #endif
 				const struct drm_mode_fb_cmd2 *mode_cmd);
